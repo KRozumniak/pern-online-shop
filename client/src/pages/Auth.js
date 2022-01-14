1bd9ddeb406cba2f34374886875da0e4fa6b77ad
Kirill Rozumniak <kirkterra@gmail.com>
2022-01-13 21:35:16 -0500

    Update

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Button, Card, Container, Form} from "react-bootstrap";
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+import {NavLink, useLocation} from "react-router-dom";
 
 const Auth = () => {
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE;
+
   return (
     <Container
       className='d-flex justify-content-center align-content-center'
       style={{position: 'relative', top: '25vh'}}
     >
       <Card style={{width: 600}} className='p-5'>
-        <h2 className='m-auto'>Authorization</h2>
+        <h2 className='m-auto'>{isLogin ? 'Login' : 'Registration'}</h2>
         <Form className='d-flex flex-column'>
           <Form.Control
             className='mt-3'
@@ -19,9 +24,20 @@ const Auth = () => {
             placeholder='Password'
             type='password'
           />
-          <Button className={'mt-3'} variant={'outline-success'}>
-            Login
-          </Button>
+          <Row className='d-flex justify-content-between'>
+            {isLogin ?
+              <div>
+              Don't have an account? <NavLink to={REGISTRATION_ROUTE}>Register!</NavLink>
+            </div>
+              :
+              <div>
+                Have an account? <NavLink to={LOGIN_ROUTE}>Login!</NavLink>
+              </div>
+            }
+              <Button className={'mt-3'} variant={'outline-success'}>
+                {isLogin ? 'Login' : 'Register'}
+              </Button>
+          </Row>
         </Form>
       </Card>
     </Container>
