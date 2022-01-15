244eae41e8cf5dd9a1f0e9ae2ec9e160bb13393e
Kirill Rozumniak <kirkterra@gmail.com>
2022-01-14 22:16:31 -0500

    Update

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,19 +2,32 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Container, Nav, Navbar, Button} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useHistory} from "react-router-dom";
 
 const NavBar = observer(() => {
   const {user} = useContext(Context);
+  const history = useHistory();
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <NavLink style={{color: 'white', textDecoration: 'none'}} to={SHOP_ROUTE}>BuyDevice</NavLink>
         {user.isAuth ?
           <Nav className="ml-auto" style={{color: 'white'}}>
-            <Button variant={'outline-light'}>Admin</Button>
-            <Button variant={'outline-light'} className='ms-2'>Logout</Button>
+            <Button
+              variant={'outline-light'}
+              onClick={() => history.push(ADMIN_ROUTE)}
+            >
+              Admin
+            </Button>
+            <Button
+              variant={'outline-light'}
+              onClick={() => history.push(LOGIN_ROUTE)}
+              className='ms-2'
+            >
+              Logout
+            </Button>
           </Nav>
           :
           <Nav className="ml-auto" style={{color: 'white'}}>
